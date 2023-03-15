5c82f94a162d8f458d30c0a9221e721293c37a63
maxawzsinger <maxawzsinger@gmail.com>
2023-03-15 23:14:10 +1100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { matchHistRow, playersRow, config, read, mainCollect } from "./helpers";
+import { DocumentData } from "firebase/firestore/lite";
+import {
+  MantineProvider,
+  Text,
+  Autocomplete,
+  TextInput,
+  NumberInput,
+  Button,
+} from "@mantine/core";
+
+const EloRating = require("elo-rating");
 
 function App() {
+  //match update form
+  const [user, setUser] = React.useState<string>(
+    localStorage.getItem("squash-keeper-default-user") ?? ""
+  );
+  const [matchesUserWon, setMatchesUserWon] = React.useState<number>(0);
+  const [matchesUserLost, setMatchesUserLost] = React.useState<number>(0);
+  const [opponent, setOpponent] = React.useState<string>("");
+
+  ///data display
+  const [matchHistory, setMatchHistory] = React.useState<
+    DocumentData | undefined
+  >();
+  const [eloRankings, setEloRankings] = React.useState<
+    playersRow[] | undefined
+  >();
+
+  const handleUserChange = (newUser: string) => {
+    localStorage.setItem("squash-keeper-default-user", newUser);
+    setUser(newUser);
+  };
+
+  const getAndSetAllData = async () => {
+    const currentPlayers = await read(mainCollect, config.playerDoc);
+    const currentHist = await read(mainCollect, config.matchHistDoc);
+    if (
+      currentHist &&
+      Array.isArray(currentHist) &&
+      currentPlayers &&
+      Array.isArray(currentPlayers)
+    ) {
+      //set current players for use by match update dropdown
+      setEloRankings(currentPlayers.sort((a, b) => a.elo - b.elo));
+      setMatchHistory(currentHist);
+    }
+  };
+  getAndSetAllData();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        {eloRankings && matchHistory ? (
+          <>
+            <Autocomplete
+              value={user}
+              onChange={(str) => handleUserChange(str)}
+              data={eloRankings.map((row) => row.name)}
+            />
+            <Text>played</Text>
+            <Autocomplete
+              value={opponent}
+              onChange={(str) => handleUserChange(str)}
+              data={eloRankings.map((row) => row.name)}
+            />
+            <Text>and won</Text>
+
+            <NumberInput
+              value={matchesUserWon}
+              onChange={(num) => setMatchesUserWon(num === "" ? 0 : num)}
+            />
+            <Text>and lost</Text>
+            <NumberInput
+              value={matchesUserWon}
+              onChange={(num) => setMatchesUserWon(num === "" ? 0 : num)}
+            />
+            <Text>matches</Text>
+
+            <Button>Submit</Button>
+          </>
+        ) : (
+          <Text>Loading...</Text>
+        )}
+      </MantineProvider>
     </div>
   );
 }
---
+++ src/helpers.ts
@@ -0,0 +1,122 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const EloRating = require("elo-rating");
+
+import {
+  getFirestore,
+  collection,
+  setDoc,
+  addDoc,
+  getDoc,
+  doc,
+  CollectionReference,
+} from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhJjDHOkhL4Mfq5O8jvHcnjdNAbYiZPmo",
+  authDomain: "squashkeeper-8f601.firebaseapp.com",
+  projectId: "squashkeeper-8f601",
+  storageBucket: "squashkeeper-8f601.appspot.com",
+  messagingSenderId: "1035659027084",
+  appId: "1:1035659027084:web:e12c3bfa348c283d5fd1a9",
+  measurementId: "G-R3ZWFFYNCX",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+export const mainCollect = collection(db, "main");
+
+type dataType = Record<string, string | number | (string | number)[]>;
+export type playersRow = { name: string; elo: number };
+type playersDocDataSchema = { data: playersRow[] };
+export type matchHistRow = {
+  playerOneName: string;
+  playerTwoName: string;
+  playerOneDidWin: boolean;
+  unixTS: number;
+  reportedBy: string;
+};
+type matchHistDocDataSchema = {
+  data: matchHistRow[];
+};
+
+export const config = {
+  playerDoc: "players",
+  matchHistDoc: "matchHist",
+};
+
+const write = async (
+  collection: CollectionReference,
+  documentName: string,
+  data: dataType
+) => {
+  await setDoc(doc(collection, documentName), data);
+};
+
+export const read = async (collection: CollectionReference, documentName: string) => {
+  const docSnap = await getDoc(doc(collection, documentName));
+  if (docSnap.exists()) {
+    return docSnap.data();
+  } else {
+    console.log("No such document!");
+  }
+};
+
+
+export const addNewPlayer = async (newPlayerName: string) => {
+  const currentPlayers = await read(mainCollect, config.playerDoc);
+  if (currentPlayers && Array.isArray(currentPlayers.data)) {
+    await write(mainCollect, config.playerDoc, {
+      data: [...currentPlayers.data, { name: newPlayerName, elo: 0 }],
+    });
+  }
+};
+
+export const updateMatchHist = async (update: matchHistRow) => {
+  const currentHist = await read(mainCollect, config.matchHistDoc);
+  const currentPlayers = await read(mainCollect, config.playerDoc);
+
+  if (
+    currentHist &&
+    Array.isArray(currentHist.data) &&
+    currentPlayers &&
+    Array.isArray(currentPlayers.data)
+  ) {
+    //update history
+    write(mainCollect, config.matchHistDoc, {
+      data: [...currentHist.data, update],
+    });
+    //update elos using
+    //https://www.npmjs.com/package/elo-rating?activeTab=explore
+    const playerOne = currentPlayers.data.find(
+      (player) => player.name === update.playerOneName
+    );
+    const playerTwo = currentPlayers.data.find(
+      (player) => player.name === update.playerTwoName
+    );
+
+    let result: any;
+    if (update.playerOneDidWin) {
+      result = EloRating.calculate(playerOne.elo, playerTwo.elo);
+    } else {
+      result = EloRating.calculate(playerOne.elo, playerTwo.elo, false);
+    }
+    await write(mainCollect, config.playerDoc, {
+      data: currentPlayers.data.map((player) => {
+        if (player.name === playerOne.name) {
+          return { ...playerOne, elo: result.playerRating };
+        }
+        if (player.name === playerTwo.name) {
+          return { ...playerTwo, elo: result.opponentRating };
+        }
+        return player;
+      }),
+    });
+  }
+};
